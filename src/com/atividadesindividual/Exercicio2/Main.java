b90b97099b462548534c07dbad76ee65dbbcdfb6
anacfsoares <anacarol.fs@hotmail.com>
2021-09-16 16:48:52 -0300

    Atividade Individual - Exercício 2

---
+++ src/com/atividadesindividual/Exercicio2/Main.java
@@ -0,0 +1,32 @@
+package com.atividadesindividual.Exercicio2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nome;
+        Scanner nomeEntrada  = new Scanner(System.in);
+        System.out.println("Entre com um nome!");
+        nome = nomeEntrada.nextLine();
+        inverteString(nome);
+        palindromo(nome);
+    }
+
+    public static void inverteString(String nome){
+        String invertido = "";
+        for (int i = nome.toCharArray().length-1; i>=0;i--){
+            invertido += nome.toCharArray()[i];
+        }
+        System.out.println(invertido.toUpperCase());
+    }
+
+    public static void palindromo(String nome){
+        String invertido = "", nomeEntrada;
+        nomeEntrada = nome.replace(" ", "");
+        for (int i = nomeEntrada.toCharArray().length-1; i>=0;i--){
+            invertido += nomeEntrada.toCharArray()[i];
+        }
+        System.out.println(nomeEntrada.toUpperCase().equals(invertido.toUpperCase())?"É palíndromo!":"Não é palíndromo!");
+    }
+}

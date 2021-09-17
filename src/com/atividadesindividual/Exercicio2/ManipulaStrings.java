c68c038d5b7b124a34cffb32db4731f85156bcc3
anacfsoares <anacarol.fs@hotmail.com>
2021-09-17 15:16:36 -0300

    Atividade Individual - Exercício 5

--- src/com/atividadesindividual/Exercicio2/ManipulaStrings.java
+++ src/com/atividadesindividual/Exercicio2/ManipulaStrings.java
@@ -14,19 +14,15 @@ public class ManipulaStrings {
     }
 
     public static void inverteString(String nome){
-        String invertido = "";
-        for (int i = nome.toCharArray().length-1; i>=0;i--){
-            invertido += nome.toCharArray()[i];
-        }
-        System.out.println(invertido.toUpperCase());
+        String nomeIn = nome.replace("","").toUpperCase();
+        StringBuilder invertido = new StringBuilder(nomeIn).reverse();
+        System.out.println(invertido);
     }
 
     public static void palindromo(String nome){
-        String invertido = "", nomeEntrada;
-        nomeEntrada = nome.replace(" ", "");
-        for (int i = nomeEntrada.toCharArray().length-1; i>=0;i--){
-            invertido += nomeEntrada.toCharArray()[i];
-        }
-        System.out.println(nomeEntrada.toUpperCase().equals(invertido.toUpperCase())?"É palíndromo!":"Não é palíndromo!");
+        String nomeEntrada;
+        nomeEntrada = nome.replace(" ", "").toUpperCase();
+        StringBuilder invertido = new StringBuilder(nomeEntrada).reverse();
+        System.out.println(nomeEntrada.equals((String) invertido.toString())?"É palíndromo!":"Não é palíndromo!");
     }
 }
